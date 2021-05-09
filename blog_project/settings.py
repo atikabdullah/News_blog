3111fdc3450e569f138fffb5f6fff4a28c4b81c1
atikabdullah <53136870+atikabdullah@users.noreply.github.com>
2021-05-09 21:29:23 +0600

    changing settings issues

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ugikr$9k$k$)8wh(5=#7!pd)5#bz8b4cxkj(47d))r#n*2b@l&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
